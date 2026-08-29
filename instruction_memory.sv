6cb517bd151633a911dd81becba567e718136996
Dangcaominh <minh.dangrandomname@hcmut.edu.vn>
2026-08-29 16:07:05 +0700

    First commit

---
+++ instruction_memory.sv
@@ -0,0 +1,28 @@
+module instruction_memory #(
+    parameter ADDR_WIDTH = 7,       // Address bits (2^7 = 128 depth)d
+    parameter DATA_WIDTH = 8        // Data width per memory cell
+) (
+    input logic imem_req,           // Read enable
+    input logic [31:0] imem_addr,   // Byte address (word-aligned)
+    output logic [31:0] imem_data   // Output instruction
+)
+
+// ----------------------------------------------
+// Memory declaration (ROM)
+// ----------------------------------------------
+
+logic [DATA_WIDTH - 1 : 0] mem [0 : (2 ** ADDR_WIDTH) - 1];
+
+initial begin
+    $readmemh("machine_code.mem", mem);     // Initialize ROM with data
+end
+
+always_comb begin
+    if(imem_reg) begin
+        imem_data = {mem[imem_addr], mem[imem_addr + 1], mem[imem_addr + 2], mem[imem_addr + 3]};
+    end else begin
+        imem_data = 32'd0;
+    end
+end
+
+endmodule
